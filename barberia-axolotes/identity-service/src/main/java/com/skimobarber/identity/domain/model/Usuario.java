e446748bf6100c17c47a7a829765da7de21cc1d5
Gus524 <e.gus.gg@gmail.com>
2026-01-04 20:40:29 -0600

    feat: Remove Lombok dependency from multiple services and update version in the main POM
    Lombok works in all microservices

--- barberia-axolotes/identity-service/src/main/java/com/skimobarber/identity/domain/model/Usuario.java
+++ barberia-axolotes/identity-service/src/main/java/com/skimobarber/identity/domain/model/Usuario.java
@@ -2,32 +2,21 @@ package com.skimobarber.identity.domain.model;
 
 import com.skimobarber.identity.domain.enums.TipoRol;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
     private Long personaId;
     private String login;
     private TipoRol rol;
     private boolean activo;
 
-    public Usuario() {}
-
-    public Usuario(Long personaId, String login, TipoRol rol, boolean activo) {
-        this.personaId = personaId;
-        this.login = login;
-        this.rol = rol;
-        this.activo = activo;
-    }
-
-    // Getters
-    public Long getPersonaId() { return personaId; }
-    public String getLogin() { return login; }
-    public TipoRol getRol() { return rol; }
     public boolean isActivo() { return activo; }
-
-    // Setters
-    public void setPersonaId(Long personaId) { this.personaId = personaId; }
-    public void setLogin(String login) { this.login = login; }
-    public void setRol(TipoRol rol) { this.rol = rol; }
-    public void setActivo(boolean activo) { this.activo = activo; }
 
     // Domain validations
     public boolean isLoginValid() {
